de000ce2bb1128c645c6105b66de9581724672e8
Jose Velarde <josevelarde00@gmail.com>
2022-03-24 19:20:04 -0300

    Added clip lookup and batch deinterlace

--- get_positive_clips.py
+++ get_positive_clips.py
@@ -450,13 +450,109 @@ def generate_clips(stations, deinterlace_flag):
         i += 1
 
 
+def print_clips_in_folder(rootdir):
+    regexfulldir = re.compile("(.*avi$)")
+    file_list = []
+    for root, dirs, files in os.walk(rootdir):
+        for file in files:
+            path = os.path.join(root, file)
+            if regexfulldir.match(path):
+
+                capture = cv2.VideoCapture(cv2.samples.findFileOrKeep(path))
+                length = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+                file_list.append([path, length])
+                # print(path, length)
+                # print(path[:110])
+                # print(path[110:-15])
+    return file_list
+
+
+def deinterlace_clips(folders, deinterlace_flag, narrow_flag):
+    deinterlace_mode = "discard"
+    # deinterlace_mode = "linear"
+    if narrow_flag == True:
+        narrow = 2
+    else:
+        narrow = 0
+
+    stacked_image = 0
+    stack_count = 6
+
+    i = 0
+    for folder in folders:
+        clips_list = print_clips_in_folder(folder)
+
+        for clip in clips_list:
+            clip_path = clip[0]
+            length = int(clip[1])
+            # print(clip_path)
+            capture = cv2.VideoCapture(cv2.samples.findFileOrKeep(clip_path))
+
+            if deinterlace_flag:
+                p = "{}Deinterlaced/{} - deinterlaced.avi".format(
+                    clip_path[:(100+narrow)], clip_path[(110+narrow):-15]
+                )
+                writer = cv2.VideoWriter(
+                    p, 0, 60, (int(capture.get(3)), int(capture.get(4)))
+                )
+            else:
+                p = "{}Clips/{} - original.avi".format(
+                    clip_path[:(100+narrow)], clip_path[(110+narrow):-15]
+                )
+
+                writer = cv2.VideoWriter(
+                    p, 0, 30, (int(capture.get(3)), int(capture.get(4)))
+                )
+            print(p)
+            for frames in range(length):
+                ret, frame = capture.read()
+                if deinterlace_flag:
+                    first_field, second_field = deinterlace(deinterlace_mode, frame)
+                    writer.write(first_field)
+                    writer.write(second_field)
+                else:
+                    # print(ret)
+                    writer.write(frame)
+                    # cv2.imshow("Frame", frame)
+                    # cv2.waitKey(-1)
+
+                    #! Get 1st frame data for delta calculations and skip to next iteration
+                    if (capture.get(cv2.CAP_PROP_POS_FRAMES)) >= (
+                        32 - stack_count
+                    ) and (capture.get(cv2.CAP_PROP_POS_FRAMES)) <= (
+                        32 + stack_count
+                    ):
+                        stacked_image += frame
+            if not deinterlace_flag:
+                cv2.imwrite(p[:-12] + "stack.png", stacked_image)
+                stacked_image = 0
+
+            writer.release()
+            cv2.destroyAllWindows()
+            capture.release()
+        i += 1
+
+
 # Stations = [Anillaco14, LaMaria02, LaMaria29, SantaMaria01, SantaMaria26, SantaMaria28]
-Stations = [Anillaco12]
+# Stations = [Anillaco12]
+Stations_Wide = [
+    "C:/Users/Rede LEONA/Downloads/Jose Downloads/OpenCV/Reviewed nights/Anillaco/2018-12-14_041911_Wide/Positives/",
+    "C:/Users/Rede LEONA/Downloads/Jose Downloads/OpenCV/Reviewed nights/Anillaco/2018-12-14_063933_Wide/Positives/",
+    "C:/Users/Rede LEONA/Downloads/Jose Downloads/OpenCV/Reviewed nights/Anillaco/2018-12-14_065708_Wide/Positives/",
+]
+
+Stations_Narrow = [
+    "C:/Users/Rede LEONA/Downloads/Jose Downloads/OpenCV/Reviewed nights/Anillaco/2018-12-14_033512_Narrow/Positives/",
+    "C:/Users/Rede LEONA/Downloads/Jose Downloads/OpenCV/Reviewed nights/Anillaco/2018-12-14_041925_Narrow/Positives/",
+    "C:/Users/Rede LEONA/Downloads/Jose Downloads/OpenCV/Reviewed nights/Anillaco/2018-12-14_065722_Narrow/Positives/",
+]
 # print_video_length()
 
 
 # deinterlace_flag = True
 deinterlace_flag = False
-
-
-generate_clips(Stations, deinterlace_flag)
+# narrow_flag= True
+narrow_flag= True
+# print_clips_in_folder(Stations[0])
+# generate_clips(Stations, deinterlace_flag)
+deinterlace_clips(Stations_Narrow, deinterlace_flag, narrow_flag)
